243df83d380fc5db0484421f632c8025f93da3f8
Rohan Rele <rohanrele@hotmail.com>
2020-06-27 00:13:41 +0530

    projects generated by spring initializer.

---
+++ rest-service-country/src/test/java/com/rohanrele/RestServiceCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohanrele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceCountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ rest-service-employee/src/main/java/com/rohanrele/RestServiceEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.rohanrele;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceEmployeeApplication.class, args);
+	}
+
+}
